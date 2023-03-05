10d565e44ef9cacc2b137e98b9b312347160a53e
Michael Robles <40410952+DogStuff-Studios@users.noreply.github.com>
2023-03-05 15:43:40 +1100

    Help me please

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import OSUUtils, GUI
 from timeit import default_timer as timer
 import customtkinter 
 import tkinter as tk
+from tkinter import messagebox
 
 import os
 # from PIL import Image
@@ -12,6 +13,34 @@ import urllib.request
 import OAuthCheck
 
 start = timer()
+
+
+client_id : int
+client_secret: str
+username : str
+user: User
+
+
+config = configparser.RawConfigParser()
+config.read('config.cfg')
+keys_dict = dict(config.items('CONFIG'))
+
+client_id = keys_dict['client_id']
+client_secret = keys_dict['client_secret']
+username = keys_dict['user']
+
+
+api = Ossapi(client_id, client_secret)
+api.scopes = [Scope.PUBLIC, Scope.IDENTIFY]
+
+try:
+    user = api.user(user=username, mode=GameMode.OSU, key=UserLookupKey.USERNAME)
+except ValueError:
+    print("USER DOES NOT EXIST, TERMINATING")
+    quit()
+
+utils = OSUUtils.Utils(api)
+
 
 class App:
     frames = {"f1": None, "f2": None}
@@ -28,7 +57,7 @@ class App:
         customtkinter.set_default_color_theme("dark-blue")
 
 
-        root.geometry("1600x800")
+        root.geometry("800x800")
         root.title("OSU Mentor Matcher!")
         root.resizable(False, False)
         # create a grid of 2 columns, 1 for sidebar, 1 for scrollable mentor list
@@ -41,6 +70,7 @@ class App:
                 # GUI.SidebarFrame.pack(fill="both", expand=True, padx=0, pady=0)
                 # GUI.LoginFrame.pack_forget()
                 # Matcher()
+                App.frames['f2'].pack()
                 App.frames['f2'].pack(fill="both", expand=True, padx=0, pady=0)
                 App.frames['f1'].pack_forget()
                 Matcher()
@@ -54,6 +84,7 @@ class App:
 
         def Login():
             login_frame = GUI.LoginFrame(master=App.frames['f1'])
+            
             App.frames['f1'].pack()
             login_frame.pack(pady=20, padx=60, fill="both", expand=True)
             login_label = customtkinter.CTkLabel(App.frames['f1'], text="Login System", font=("Roboto", 24))
@@ -73,6 +104,8 @@ class App:
             login_button = customtkinter.CTkButton(App.frames['f1'], text="Get Osu OAuth", command=OAuthCheck.new_OAuth)
             login_button.pack(pady=12, padx=14)
 
+            logo_image = customtkinter.CTkImage(Image.open("osuIconRevised.png"), size=(26, 26))
+
         
 
         
@@ -81,49 +114,52 @@ class App:
             # # load images with light and dark mode image
             # image_file = "osuIconRevised.png"
             # image = tk.PhotoImage(file = image_file)
-
+            App.frames['f2'].pack()
+            matcher_frame = GUI.ScrollableMentorFrame(master=App.frames['f2'])
             sidebar_frame = GUI.SidebarFrame(master=App.frames['f2'])
-            sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
-            # sidebar_frame.grid_propagate(False)
-            sidebar_frame.grid_rowconfigure(4, weight=1) # stretches sidebar to the bottom of the window
 
-            lblScrapeLimit = customtkinter.CTkLabel(App.frames['f2'],text = "Scraping Limit:")
-            lblScrapeLimit.place(x=50,y=100)
-            txtScrapingLimit = customtkinter.CTkEntry(App.frames['f2'])
-            txtScrapingLimit.place(x=50,y=125)
+            
 
-            lblStartRank = customtkinter.CTkLabel(App.frames['f2'],text = "Starting Rank:")
-            lblStartRank.place(x=50,y=175)
-            txtStartRank = customtkinter.CTkEntry(App.frames['f2'])
-            txtStartRank.place(x=50,y=200)
+            sidebar_label = customtkinter.CTkLabel(App.frames['f2'], text="Sidebar!", font=("Roboto", 24))
+            sidebar_label.grid(row=0, column=0, padx=90, pady=(20, 20))
 
-            lblEndRank = customtkinter.CTkLabel(App.frames['f2'],text = "Ending Rank:")
-            lblEndRank.place(x=50,y=250)
-            txtEndRank = customtkinter.CTkEntry(App.frames['f2'])
-            txtEndRank.place(x=50,y=275)
-            def btnEnter():
-                scraping_limit = int(txtScrapingLimit.get())
-                starting_rank = int(txtStartRank.get())
-                ending_rank = int(txtEndRank.get())
-                if scraping_limit < 1 :
+            entry_top_played_beatmaps_scraping_limit = customtkinter.CTkEntry(App.frames['f2'], width=250, placeholder_text="Top Played Beatmaps Scraping Limit")
+            entry_top_played_beatmaps_scraping_limit.grid(row=1, column=0, pady=(0, 20))
+
+            entry_start_rank = customtkinter.CTkEntry(App.frames['f2'], width=250, placeholder_text= "Starting Rank")
+            entry_start_rank.grid(row=2, column=0, pady=(0, 20))
+
+            entry_end_rank = customtkinter.CTkEntry(App.frames['f2'], width=250, placeholder_text="Ending Rank")
+            entry_end_rank.grid(row=3, column=0, pady=(0, 20))
+
+            entry_playstyle_scraping_limit = customtkinter.CTkEntry(App.frames['f2'], width=250, placeholder_text="Playstyle Scraping Limit")
+            entry_playstyle_scraping_limit.grid(row=4, column=0, pady=(0, 20))
+
+            def set_inputs():
+                top_played_beatmaps_scraping_limit = int(entry_top_played_beatmaps_scraping_limit.get())
+                starting_rank = int(entry_start_rank.get())
+                ending_rank = int(entry_end_rank.get())
+                if top_played_beatmaps_scraping_limit < 1 :
                     messagebox.showerror("Error", "Invalid Scraping Limit!")
                 elif starting_rank < 1:
                     messagebox.showerror("Error", "Invalid Starting Rank!")
-                elif scraping_limit < 1 and starting_rank < 1 : 
+                elif top_played_beatmaps_scraping_limit < 1 and starting_rank < 1 : 
                     messagebox.showerror("Error", "Invalid Scraping Limit and Starting Rank!")
 
-                
-            btnEnter = customtkinter.CTkButton(App.frames['f2'], text = "Enter", command=btnEnter)
-            btnEnter.place(x=50,y=350)
+            
+            enter_button = customtkinter.CTkButton(App.frames['f2'], text = "Enter", command=set_inputs)
+            enter_button.grid(row=5, column=0)
 
             def label_button_frame_event(item):
                 print(f"label button frame clicked: {item}")
 
-            scrollable_mentor_frame = GUI.ScrollableMentorFrame(master=App.frames['f2'], width=1250, command=label_button_frame_event, corner_radius=0)
-            scrollable_mentor_frame.grid(row=0, rowspan=4, column=1, padx=40, pady=0, sticky="nsew")
+            scrollable_mentor_frame = GUI.ScrollableMentorFrame(master=root, width=1250, command=label_button_frame_event, corner_radius=0)
+            scrollable_mentor_frame.grid(row=0, rowspan=5, column=1, padx=40, pady=0, sticky="nsew")
 
             for i in range(20):  # add items with images
-                scrollable_mentor_frame.add_item(f"Username {i}", image=None)
+                scrollable_mentor_frame.add_mentor(f"Username {i}", f"Similarity Score: {i}", pfp=None)
+
+
 
         Login()
        
