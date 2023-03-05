3aacb2465138efdbc64cd3962d5a8e27cf5a2904
DanielArnould <darn0005@student.monash.edu>
2023-03-05 12:30:15 +1100

    FIRST GUI TEST

---
+++ GUI.py
@@ -0,0 +1,51 @@
+import customtkinter
+
+class LoginFrame(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.login_label = customtkinter.CTkLabel(self, text="Login System", font=("Roboto", 24))
+        self.login_label.pack(pady=12, padx=10)
+
+        self.client_id_entry = customtkinter.CTkEntry(self, placeholder_text="Client ID", show="*")
+        self.client_id_entry.pack(pady=12, padx=10)
+
+        self.client_key_entry = customtkinter.CTkEntry(self, placeholder_text="Client Secret", show="*")
+        self.client_key_entry.pack(pady=12, padx=10)
+
+        self.username_entry = customtkinter.CTkEntry(self, placeholder_text="username")
+        self.username_entry.pack(pady=12, padx=10)
+
+        login_button = customtkinter.CTkButton(self, text="Login", command=login)
+        login_button.pack(pady=12, padx=12)
+
+        # checkbox = customtkinter.CTkCheckBox(self, text="Remember Me")
+        # checkbox.pack(pady=12, padx=10)
+
+class SidebarFrame(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master, width=300, corner_radius=0)
+
+        self.login_label = customtkinter.CTkLabel(self, text="Login System", font=("Roboto", 24))
+        self.login_label.grid(row=0, column=0)
+
+
+class ScrollableMentorFrame(customtkinter.CTkScrollableFrame):
+    def __init__(self, master):
+        super().__init__(self, master, label_text="CTKScrollabelFrame")
+
+        self.grid(row=1, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.grid_columnconfigure(0, weight=1)
+        self.scrollable_frame_switches = []
+        for i in range(100):
+            switch = customtkinter.CTkSwitch(master=self, text=f"CTkSwitch {i}")
+            switch.grid(row=i, column=0, padx=10, pady=(0, 20))
+            self.scrollable_frame_switches.append(switch)
+
+
+
+
+
+def login():
+    print("Test")
+
--- main.py
+++ main.py
@@ -1,9 +1,58 @@
 from ossapi import *
 import configparser
-import OSUUtils
+import OSUUtils, GUI
 from timeit import default_timer as timer
+import customtkinter
+
+
 
 start = timer()
+
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("green")
+
+root = customtkinter.CTk()
+root.geometry("1600x800")
+root.title("OSU Mentor Matcher!")
+# create a grid of 2 columns, 1 for sidebar, 1 for scrollable mentor list
+# possibly could change column weights to 1 and 3
+root.grid_columnconfigure((0, 1), weight=0)
+root.grid_rowconfigure((0, 1, 2, 3), weight=1)
+
+
+
+
+# login_frame = GUI.LoginFrame(master=root)
+# login_frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+sidebar_frame = GUI.SidebarFrame(master=root)
+sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+sidebar_frame.grid_rowconfigure(4, weight=1)
+
+# scrollable_mentor_frame = GUI.ScrollableMentorFrame(master=root)
+
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 """"IMPORTANT NOTES:
 Beatmap Set = All the associated beatmaps by an artist, for all difficulties, for a specific song.
@@ -16,27 +65,27 @@ Beatmaps in set are ordered from hardest difficulty to easiest difficulty
 # Client ID is needed for operation, client-secret allows for user control
 # Create a new client at https://osu.ppy.sh/home/account/edit#oauth
 
-config = configparser.RawConfigParser()
-config.read('config.cfg')
-keys_dict = dict(config.items('CONFIG'))
+# config = configparser.RawConfigParser()
+# config.read('config.cfg')
+# keys_dict = dict(config.items('CONFIG'))
 
-client_id = keys_dict['client_id']
-client_secret = keys_dict['client_secret']
+# client_id = keys_dict['client_id']
+# client_secret = keys_dict['client_secret']
 
 
-api = Ossapi(client_id, client_secret)
-api.scopes = [Scope.PUBLIC, Scope.IDENTIFY]
-user = api.user(user = keys_dict['user'], mode=GameMode.OSU)
-utils = OSUUtils.Utils(api)
+# api = Ossapi(client_id, client_secret)
+# api.scopes = [Scope.PUBLIC, Scope.IDENTIFY]
+# user = api.user(user = keys_dict['user'], mode=GameMode.OSU)
+# utils = OSUUtils.Utils(api)
 
-user_playstyle = utils.get_playstyle(user, scraping_limit=3)
+# user_playstyle = utils.get_playstyle(user, scraping_limit=3)
 
-# Looks at a user's top 10 played beatmaps, finds rank 1-4 players in each of them, gathers play style data based on their top 3 most played beatmaps
-common_player_playstyles = utils.get_common_player_playstyles(user,
-                                                        top_played_beatmaps_count=10, starting_rank=1,
-                                                        ending_rank=3, playstyle_scraping_limit=3)
+# # Looks at a user's top 10 played beatmaps, finds rank 1-4 players in each of them, gathers play style data based on their top 3 most played beatmaps
+# common_player_playstyles = utils.get_common_player_playstyles(user,
+#                                                         top_played_beatmaps_count=10, starting_rank=1,
+#                                                         ending_rank=3, playstyle_scraping_limit=3)
 
-print(utils.get_playstyle_similarties(base_playstyle=user_playstyle, comparison_playstyles=common_player_playstyles))
+# print(utils.get_playstyle_similarties(base_playstyle=user_playstyle, comparison_playstyles=common_player_playstyles))
 
 
 end = timer()
