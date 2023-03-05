ccfeb72b86789c58c0fbb68892af52d6380fe4d3
Michael Robles <40410952+DogStuff-Studios@users.noreply.github.com>
2023-03-05 15:44:08 +1100

    PLease Dear God

--- GUI.py
+++ GUI.py
@@ -15,10 +15,7 @@ class LoginFrame(customtkinter.CTkFrame):
 
 class SidebarFrame(customtkinter.CTkFrame):
     def __init__(self, master):
-        super().__init__(master, width=300, corner_radius=0)
-
-        self.login_label = customtkinter.CTkLabel(self, text="Sidebar!", font=("Roboto", 24))
-        self.login_label.grid(row=0, column=0, padx=50)
+        super().__init__(master)
 
 
 class ScrollableMentorFrame(customtkinter.CTkScrollableFrame):
@@ -27,27 +24,22 @@ class ScrollableMentorFrame(customtkinter.CTkScrollableFrame):
 
         self.command = command
         self.radiobutton_variable = customtkinter.StringVar()
-        self.label_list = []
+        self.username_list = []
         self.button_list = []
+        self.similarity_scores =[]
 
-    def add_item(self, item, image=None):
-        label = customtkinter.CTkLabel(self, text=item, image=image, compound="left", padx=5, anchor="w")
+    def add_mentor(self, username, similarity_score, pfp=None):
+        username_label = customtkinter.CTkLabel(self, text=username, image=pfp, compound="left", padx=5, anchor="w")
         button = customtkinter.CTkButton(self, text="See Profile", width=100, height=24)
+        similarity_label = customtkinter.CTkLabel(self, text=similarity_score, padx=20, anchor="e")
         if self.command is not None:
-            button.configure(command=lambda: self.command(item))
-        label.grid(row=len(self.label_list), column=0, pady=(0, 20), sticky="w")
-        button.grid(row=len(self.button_list), column=1, pady=(0, 20), padx=10)
-        self.label_list.append(label)
+            button.configure(command=lambda: self.command(username))
+        username_label.grid(row=len(self.username_list), column=0, pady=(0, 20), sticky="w")
+        similarity_label.grid(row=len(self.similarity_scores), column=1, pady=(0, 20), sticky="e")
+        button.grid(row=len(self.button_list), column=2, pady=(0, 20), padx=10)
+        self.username_list.append(username_label)
+        self.similarity_scores.append(similarity_label)
         self.button_list.append(button)
-
-    def remove_item(self, item):
-        for label, button in zip(self.label_list, self.button_list):
-            if item == label.cget("text"):
-                label.destroy()
-                button.destroy()
-                self.label_list.remove(label)
-                self.button_list.remove(button)
-                return
 
 
 
---
+++ mainnew.py
@@ -0,0 +1,90 @@
+from ossapi import *
+import configparser
+import OSUUtils, GUI
+from timeit import default_timer as timer
+import customtkinter 
+
+import os
+from PIL import Image
+import urllib.request
+
+
+
+start = timer()
+
+client_id : int
+client_secret: str
+username : str
+user: User
+
+config = configparser.RawConfigParser()
+config.read('config.cfg')
+keys_dict = dict(config.items('CONFIG'))
+
+client_id = keys_dict['client_id']
+client_secret = keys_dict['client_secret']
+username = keys_dict['user']
+
+
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("dark-blue")
+
+root = customtkinter.CTk()
+root.geometry("800x800")
+root.title("OSU Mentor Matcher!")
+root.resizable(False, False)
+root.deiconify()
+# create a grid of 2 columns, 1 for sidebar, 1 for scrollable mentor list
+# possibly could change column weights to 1 and 3
+root.grid_columnconfigure((0, 1), weight=0)
+root.grid_rowconfigure((0, 1, 2, 3), weight=1)
+
+
+
+
+# login_frame = GUI.LoginFrame(master=root)
+# login_frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+
+
+
+api = Ossapi(client_id, client_secret)
+api.scopes = [Scope.PUBLIC, Scope.IDENTIFY]
+
+try:
+    user = api.user(user=username, mode=GameMode.OSU, key=UserLookupKey.USERNAME)
+except ValueError:
+    print("USER DOES NOT EXIST, TERMINATING")
+    quit()
+
+utils = OSUUtils.Utils(api)
+
+
+
+# user_playstyle = utils.get_playstyle(user, scraping_limit=3)
+
+# # Looks at a user's top 10 played beatmaps, finds rank 1-4 players in each of them, gathers play style data based on their top 3 most played beatmaps
+# common_player_playstyles = utils.get_common_player_playstyles(user,
+#                                                         top_played_beatmaps_count=10, starting_rank=1,
+#                                                         ending_rank=3, playstyle_scraping_limit=3)
+
+# print(utils.get_playstyle_similarties(base_playstyle=user_playstyle, comparison_playstyles=common_player_playstyles))
+
+
+sidebar_frame = GUI.SidebarFrame(master=root)
+sidebar_frame.grid(row=0, column=0, rowspan=5, sticky="nsew")
+# sidebar_frame.grid_rowconfigure(4, weight=1) # stretches sidebar to the bottom of the window
+
+
+
+def label_button_frame_event(item):
+    print(f"label button frame clicked: {item}")
+
+scrollable_mentor_frame = GUI.ScrollableMentorFrame(master=root, width=1250, command=label_button_frame_event, corner_radius=0)
+scrollable_mentor_frame.grid(row=0, rowspan=5, column=1, padx=40, pady=0, sticky="nsew")
+
+for i in range(20):  # add items with images
+    scrollable_mentor_frame.add_mentor(f"Username {i}", f"Similarity Score: {i}", pfp=None)
+
+
+root.mainloop()
