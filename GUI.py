26300453504573b66549bf472df54aa9ce1bc920
DanielArnould <darn0005@student.monash.edu>
2023-03-05 13:42:33 +1100

    Added scrollable list

--- GUI.py
+++ GUI.py
@@ -26,21 +26,37 @@ class SidebarFrame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master, width=300, corner_radius=0)
 
-        self.login_label = customtkinter.CTkLabel(self, text="Login System", font=("Roboto", 24))
-        self.login_label.grid(row=0, column=0)
+        self.login_label = customtkinter.CTkLabel(self, text="Sidebar!", font=("Roboto", 24))
+        self.login_label.grid(row=0, column=0, padx=50)
 
 
 class ScrollableMentorFrame(customtkinter.CTkScrollableFrame):
-    def __init__(self, master):
-        super().__init__(self, master, label_text="CTKScrollabelFrame")
+    def __init__(self, master, command=None, **kwargs):
+        super().__init__(master, **kwargs)
 
-        self.grid(row=1, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
-        self.grid_columnconfigure(0, weight=1)
-        self.scrollable_frame_switches = []
-        for i in range(100):
-            switch = customtkinter.CTkSwitch(master=self, text=f"CTkSwitch {i}")
-            switch.grid(row=i, column=0, padx=10, pady=(0, 20))
-            self.scrollable_frame_switches.append(switch)
+        self.command = command
+        self.radiobutton_variable = customtkinter.StringVar()
+        self.label_list = []
+        self.button_list = []
+
+    def add_item(self, item, image=None):
+        label = customtkinter.CTkLabel(self, text=item, image=image, compound="left", padx=5, anchor="w")
+        button = customtkinter.CTkButton(self, text="See Profile", width=100, height=24)
+        if self.command is not None:
+            button.configure(command=lambda: self.command(item))
+        label.grid(row=len(self.label_list), column=0, pady=(0, 20), sticky="w")
+        button.grid(row=len(self.button_list), column=1, pady=(0, 20), padx=10)
+        self.label_list.append(label)
+        self.button_list.append(button)
+
+    def remove_item(self, item):
+        for label, button in zip(self.label_list, self.button_list):
+            if item == label.cget("text"):
+                label.destroy()
+                button.destroy()
+                self.label_list.remove(label)
+                self.button_list.remove(button)
+                return
 
 
 
--- main.py
+++ main.py
@@ -3,17 +3,20 @@ import configparser
 import OSUUtils, GUI
 from timeit import default_timer as timer
 import customtkinter
+from PIL import Image
+import urllib.request
 
 
 
 start = timer()
 
 customtkinter.set_appearance_mode("system")
-customtkinter.set_default_color_theme("green")
+customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.CTk()
 root.geometry("1600x800")
 root.title("OSU Mentor Matcher!")
+root.resizable(False, False)
 # create a grid of 2 columns, 1 for sidebar, 1 for scrollable mentor list
 # possibly could change column weights to 1 and 3
 root.grid_columnconfigure((0, 1), weight=0)
@@ -27,9 +30,21 @@ root.grid_rowconfigure((0, 1, 2, 3), weight=1)
 
 sidebar_frame = GUI.SidebarFrame(master=root)
 sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
-sidebar_frame.grid_rowconfigure(4, weight=1)
+# sidebar_frame.grid_propagate(False)
+sidebar_frame.grid_rowconfigure(4, weight=1) # stretches sidebar to the bottom of the window
 
-# scrollable_mentor_frame = GUI.ScrollableMentorFrame(master=root)
+def label_button_frame_event(item):
+    print(f"label button frame clicked: {item}")
+
+scrollable_mentor_frame = GUI.ScrollableMentorFrame(master=root, width=1250, command=label_button_frame_event, corner_radius=0)
+scrollable_mentor_frame.grid(row=0, rowspan=4, column=1, padx=40, pady=0, sticky="nsew")
+
+for i in range(20):  # add items with images
+    scrollable_mentor_frame.add_item(f"Username {i}", image=None)
+
+
+
+
 
 root.mainloop()
 
